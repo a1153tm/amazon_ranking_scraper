1b0f848d98c15a1d8703fb2a1495591df5ae72a8
Taiji Miyabe <a1153tm@aiit.ac.jp>
2016-01-16 23:16:04 +0900

    add last try process

--- scrape_mnrate.rb
+++ scrape_mnrate.rb
@@ -259,9 +259,9 @@ browser = Browser.new config, logger
 
 in_str = open("#{__dir__}/asin.txt") { |f| f.read.chomp }
 result = {
-  success: 0,
-  warning: 0,
-  error: 0
+  success: [],
+  warning: [],
+  error: []
 }
 in_str.split(/\n/).each do |line|
   asin = line.chomp
@@ -274,23 +274,45 @@ in_str.split(/\n/).each do |line|
       out_file = "#{out_dir}/#{asin}.csv"
       write_to_csv(asin, hist_data, out_file)
     end
-    result[:success] += 1
+    result[:success] << asin
     logger.info("#{asin} successfull.")
   rescue NotFoundError => ex
-    result[:warning] += 1
+    result[:warning] << asin
     logger.warn("#{asin} #{ex.message}")
   rescue => ex
     logger.error("#{asin} failed.")
     logger.error(ex.to_s)
     logger.error(ex.backtrace.join("\n"))
-    result[:error] += 1
+    result[:error] << asin
     if ex.is_a? SiteUnkownError
       browser.save_screenshot "#{log_dir}/#{asin}.png"
     end
   end
 end
 
+result[:error].each do |asin|
+  logger.info("Last try for #{asin}.")
+  begin
+    hist_data = get_hist_data(browser, asin)
+    if config['format'] == 'excel'
+      out_file = "#{out_dir}/#{asin}.xlsx"
+      write_to_excel(asin, hist_data, out_file)
+    else
+      out_file = "#{out_dir}/#{asin}.csv"
+      write_to_csv(asin, hist_data, out_file)
+    end
+    result[:success] << asin
+    result[:error].delete asin
+    logger.info("#{asin} successfull.")
+  rescue SiteUnkownError => ex
+      logger.error("#{asin} failed.")
+      logger.error(ex.to_s)
+      logger.error(ex.backtrace.join("\n"))
+  end
+end
+
 browser.quit
 
 logger.info "============= scrape_mnrate finished!! ============"
-logger.info result
+p result
+logger.info result.map {|s, asin| "#{s}: #{asin.size}"}.join(", ")
